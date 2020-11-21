fc3a69b895c7abc63177c4be3955d3a94a7affed
nguyenmaianh249 <74438856+nguyenmaianh249@users.noreply.github.com>
2020-11-22 00:25:17 +0700

    Update and rename debug.log to bai25.1.js

---
+++ bai25.1.js
@@ -0,0 +1,19 @@
+* Tạo một đối tượng student gồm các property name, sex, age
+ * Example:
+  name: "CodersX",
+  sex: "Male",
+  age: 1
+ * Viết hàm showInfo trả về object đó
+*/
+
+var student = {}
+
+function showInfo(obj) {
+    this .name = name;
+    this.sex = sex;
+    this . age = age;
+  for ( var key in obj)
+  if (key=== prop)
+  return student[key ]
+  return student 
+}
